44367df374b1562550372d7c779d0cfd8d940f41
Michael Nguyen <michaelphung.nguyen@gmail.com>
2015-10-07 19:10:47 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,96 @@
+var database = {
+    
+    object:{
+        0:{
+            name: "University of Missouri - Kansas City",
+            catID: 0    
+        },
+        1:{
+            name: "Missouri University",
+            catID: 0
+        }
+    },
+    
+    dataset:{
+        0:{
+            value: 22188,
+            objID: 0,
+            attID: 0
+        },
+        1:{
+            value: 19647,
+            objID: 1,
+            attID: 0
+        },
+        2:{
+            value: 34935,
+            objID: 0,
+            attID: 1
+        },
+        3:{
+            value: 34000,
+            objID: 1,
+            attID: 1
+        },
+        4:{
+            value: 15259,
+            objID: 0,
+            attID: 2
+        },
+        5:{
+            value: 32341,
+            objID: 1,
+            attID: 2
+        },
+        6:{
+            value: 467007,
+            objID: 0,
+            attID: 3
+        },
+        7:{
+            value: 115276,
+            objID: 1,
+            attID: 3
+        },
+        8:{
+            value: 56,
+            objID: 0,
+            attID: 4
+        },
+        9:{
+            value: 81,
+            objID: 1,
+            attId: 4
+        },
+        10:{
+            value: 35,
+            objID: 0,
+            attID: 5
+        },
+        11:{
+            value: 69,
+            objId: 1,
+            attID: 5
+        }
+    },
+    
+    attribute:{
+        0:{
+            name: "in-state tuition",
+            warning: false,
+            importance: 5
+        },
+        1:{
+            name: "Out-of-State Tuition",
+            warning: true,
+            importance: 5
+        }
+    },
+    
+    category:{
+        0:{
+            name: "colleges",
+        }
+    }
+    
+}
